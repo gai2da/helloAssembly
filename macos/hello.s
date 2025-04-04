ea2d361255f8c5568f3e009688368d2806d1321a
gai2da <ghaidaalsaadi23@gmail.com>
2025-04-04 09:12:28 +0100

    Initial commit

--- macos/hello.s
+++ macos/hello.s
@@ -6,7 +6,7 @@ _main:
     add  x0, x0, msg@PAGEOFF
     bl _puts         // print message
 
-    mov x0, #0       
+    mov x0, #0        
     bl _exit         // exit
 
     .section __TEXT,__text
